8b12362df262f6031fcaa8a46a1015309ba1a4e9
oscarjsv <gumball_junior@hotmail.com>
2020-03-17 19:49:04 -0500

    second_operation

---
+++ seconds_operations.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_d_i - print decimal int
+ * @ap: argument
+ * Return: i
+ */
+int print_d_i(valist ap)
+{
+	int n, i;
+	char *s;
+
+	n = va_arg(ap, int);
+	s = malloc(sizeof(char *));
+
+	_itoa(n, s, 10);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		print_principal(s[i]);
+	}
+	free(s);
+	return (i);
+}
