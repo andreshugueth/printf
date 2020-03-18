a94291f089c9d8edbe295c87b97412333ebf4b7a
oscarjsv <gumball_junior@hotmail.com>
2020-03-17 19:27:14 -0500

    _itoafunction

---
+++ _itoa.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string.
+ * @s: string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	for (i = 0; s[i] != '\0' i++)
+	{
+	}
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
+
+}
+/**
+ * _itoa - reverses a string.
+ * @str: string
+ * @num: numbers
+ * @base:base 10
+ * Return: Always 0.
+ */
+char *_itoa(int num, char *str, int base)
+{
+	int i, rem, neg;
+
+	i = neg = 0;
+
+	if (num == 0)
+	{
+		str[i++] = '0';
+		str[i] = '\0';
+		return (str);
+	}
+	if (num < 0 && base == 10)
+	{
+		neg = 1;
+		num = num * -1;
+	}
+}
